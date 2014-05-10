2698bed3ebaf2b6e60abaabb7fdab6ddcf530102
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-10 12:09:36 -0500

    Saturday work: creating an array, printing elements of the array with while loop.

---
+++ test_if_loop.php
@@ -0,0 +1,15 @@
+<?php
+
+$name=["Greg", "Andrea", "Lou", "Johnny", "Ray"];
+$a = 0;
+
+while ($a <= 4){
+	echo $a.PHP_EOL;
+	print_r($name[$a]);
+	echo PHP_EOL;
+	$a++;
+}
+
+echo "Done!".PHP_EOL;
+
+?>
