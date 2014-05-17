724b82c318da6871507b9796b690ac8a76ae3a92
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-17 17:10:57 -0500

    Refactored code

--- blackjackhand.php
+++ blackjackhand.php
@@ -1,48 +1,53 @@
 <?php
 
-$hand = array('A-H', '5-D', 'K-C', 'A-S', '4-H');
+$hand = array( '5-D', 'K-C', 'A-S', 'A-H', '4-H');
 
 function getTotal($hand)
 {
     $total = 0;
-
+    $aceCount = 0;
     // loop through hand and calculate total value
     // use "explode" function to separate card suit and value
     // aces count as 11 unless you are over 21 and then they count as 1
     // K, Q, and J count as 10
     // numeric cards count as their value
 
+    
+    
 
     foreach ($hand as $card)
     {
         explode('-', $card);
         
-        
+        // set value of cards, rather than do addition
         switch ($card) {
             case ($card[0] == "A"):
-                $total += 11;
-                if ($total > 21); {
-                    $total -= 10;
-                } break;
-            case ($card[0] == "K"):
-                $total += 10;
+                $aceCount += 1;
+                $cardValue = 11;
+                if ($total > 21) {
+                    if ($aceCount == 1) {
+                        $total -+ 10;
+                    } elseif ($aceCount >= 2) {
+                        $total = $total - $aceCount * 10;
+                    }
+                }
                 break;
-            case ($card[0] == "Q"):
-                $total += 10;
-                break;
-            case ($card[0] == "J"):
-                $total += 10;
+            case ($card[0] == "K" || $card[0] == 'Q' || $card[0] == 'J'):
+                $cardValue = 10;
                 break;
             default:
-                $total += ($card[0]);
+                $cardValue = ($card[0]);
                 break;
         }
+
+        $total += $cardValue;
+        
     }
 
     return $total;
-    if ($total == 21) {
-        
-    }
+
+    
+
 }
 
 echo getTotal($hand) . PHP_EOL;
